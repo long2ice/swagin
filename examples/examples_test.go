5a07ac78b2c1b385b3794c87d8b2e985dfbd0ee2
Michel Casabianca <michel.casabianca@intercloud.com>
2022-03-29 14:23:22 +0200

    Added integration test

---
+++ examples/examples_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestIntegration(t *testing.T) {
+	go main()
+	WaitServer()
+	out, err := exec.Command("venom", "run", "test/query.yml").Output()
+	if err != nil {
+		t.Fatalf("running venom: %s", string(out))
+	}
+}
+
+func WaitServer() {
+	timeout := 10 * time.Millisecond
+	for {
+		conn, err := net.DialTimeout("tcp", "127.0.0.1:8080", timeout)
+		if err == nil {
+			conn.Close()
+			return
+		}
+		time.Sleep(timeout)
+	}
+}
